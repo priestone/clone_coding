e0c8408084a50e985fe8166e5637ab756baa8168
사재원 <aion97@naver.com>
2024-10-06 19:39:00 +0900

    헤더 화면구현
    메뉴 마우스오버 스크립트 구현 완료,
    섹션2 마진중첩 현상 발생

--- KCC이지스/javaScript/script.js
+++ KCC이지스/javaScript/script.js
@@ -24,3 +24,27 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
       }
   });
+
+  const menuWrapEl = document.querySelector(`.menu_wrap`);
+  const subWrapEl = document.querySelectorAll(`.sub_menu_wrap`);
+  const menuEl = document.querySelectorAll(`.menu`);
+  const submenuEl = document.querySelectorAll(`.sub_menu`);
+
+  for(let i = 0; i < menuEl.length; i++){
+    menuEl[i].addEventListener(`mouseover`,() => {
+      subWrapEl[i].classList.add(`active`);
+    })
+  
+    menuEl[i].addEventListener(`mouseout`,() => {
+      subWrapEl[i].classList.remove(`active`);
+    })
+
+    subWrapEl[i].addEventListener(`mouseover`,() => {
+      subWrapEl[i].classList.add(`active`);
+    })
+  
+    subWrapEl[i].addEventListener(`mouseout`,() => {
+      subWrapEl[i].classList.remove(`active`);
+    })
+  }
+
